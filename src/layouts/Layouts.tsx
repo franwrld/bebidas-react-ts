2da27b42b34659b98b7c3e4a2b227e7d54c9bf46
franwrld <franwrld22@gmail.com>
2025-04-11 09:09:11 -0600

    Primer Commit Bebidas

---
+++ src/layouts/Layouts.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Header from "../components/Header"
+
+export default function Layouts() {
+  return (
+    <>
+        <Header />
+        <main className="container mx-auto py-16">
+            <Outlet />
+        </main>
+        
+    </>
+  )
+}
---
+++ src/views/FavoritesPage.tsx
@@ -0,0 +1,9 @@
+
+
+export default function FavoritesPage() {
+  return (
+    <>
+        <h1>Favs</h1>
+    </>
+  )
+}
