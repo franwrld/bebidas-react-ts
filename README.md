ab22f86a5cc5f80f83a7935960881501269a5214
franwrld <franwrld22@gmail.com>
2025-04-11 09:20:18 -0600

    Resaltado de pagina hecho con NavLink

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React + TypeScript + Vite
+Comandos usados
+npm i react-router-dom
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-
+import { NavLink } from "react-router-dom"
 
 export default function Header() {
   return (
@@ -8,8 +8,23 @@ export default function Header() {
                 <div>
                     <img className="w-32" src="/logo.svg" alt="logotipo" />
                 </div>
-                <nav>
-
+                <nav className="flex gap-4">
+                    <NavLink 
+                        to="/"
+                        className={({isActive}) =>
+                            isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
+                        }
+                    >
+                        Inicio
+                    </NavLink>
+                    <NavLink 
+                        to="/favoritos"
+                        className={({isActive}) =>
+                            isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
+                        }
+                    >
+                        Favoritos
+                    </NavLink>
                 </nav>
             </div>
         </div>
